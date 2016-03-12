84ac20071b0a64ba311807b5eeb7502394384ed1
chibill <chibill110@gmail.com>
2016-03-11 23:47:24 -0600

    Fixed not working properly in 1.9, Broke 1.8 backwards compatability. Will be fixed in the next release.

--- src/main/java/chibill/slabs/Main.java
+++ src/main/java/chibill/slabs/Main.java
@@ -1,83 +1,64 @@
 package chibill.slabs;
 
 
-import java.util.logging.Logger;
-
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemFlag;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import chibill.slabs.slabs.SlabEventHandler;
 
+
+//Not safe untested
+
 public class Main extends JavaPlugin {
 
-	Logger logger = this.getLogger();
-	@Override
-	public void onEnable() {
-		SlabEventHandler handler = new SlabEventHandler();
-		this.getServer().getPluginManager().registerEvents(handler , this);
-	}
-
-	@Override
-	public void onDisable() {
+		@Override
+	    public void onEnable() {
+			Listener SlabEventHandler = new SlabEventHandler();
+			getServer().getPluginManager().registerEvents(SlabEventHandler , this);
+	    }
+	 
+	    @Override
+	    public void onDisable() {
+	        
+	    }
 	
-	}
-
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(cmd.getName().equalsIgnoreCase("slab") && sender.hasPermission("Slab.slab")){
-			if((sender instanceof Player)){
-			ItemStack item = sender.getServer().getPlayer(sender.getName()).getItemInHand();
-			if(isSlab(item.getData())){
-				sender.getServer().getPlayer(sender.getName()).getInventory().remove(item);
-				ItemMeta meta = item.getItemMeta();
-				meta.setDisplayName("Upside down slab");
-				meta.addEnchant(Enchantment.DURABILITY, 1, false);
-				item.setItemMeta(meta);
-				sender.getServer().getPlayer(sender.getName()).getInventory().addItem(item);
-				sender.sendMessage("Your requested slab is now in your hand");
-			}else{
-				if(((Player) sender).getGameMode() != GameMode.SURVIVAL){
-				ItemStack item1 = new ItemStack(Material.STEP);
-				ItemMeta meta = item1.getItemMeta();
-				meta.setDisplayName("Upside down slab");
-				meta.addEnchant(Enchantment.DURABILITY, 1, false);
-				item1.setItemMeta(meta);
-				sender.getServer().getPlayer(sender.getName()).getInventory().addItem(item1);
-				sender.sendMessage("Your requested slab has been added inventory.");
-				}else{
-					sender.sendMessage("You must have a slab in your hand to get an Upside DOwn salb in Survival");
-				}}
-			}else{
-				sender.sendMessage("This command can only be ran from ingame.");
-			}
-			return true;
-		}else{
-			return false;
-		}
-	}
-
-	public static boolean isSlab(MaterialData mat){
-			try{
-				mat.getClass().getMethod("isInverted", null);
-				try{
-					mat.getClass().getMethod("getFacing", null);
-				}catch(Exception e){
-					
-					return true;
-				}
-			}catch(Exception e){
-				
-			}
-		return false;
-	}
-
+	    @Override
+	    	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	    		if(cmd.getName().equalsIgnoreCase("slab") && sender.hasPermission("Slab.slab")){
+	    			if((sender instanceof Player)){
+	    			ItemStack item = sender.getServer().getPlayer(sender.getName()).getItemInHand();
+	    			if(item.getType().equals(Material.STEP) | item.getType().equals(Material.WOOD_STEP) | item.getType().equals(Material.STONE_SLAB2)| item.getType().equals(Material.PURPUR_SLAB)){
+	    				sender.getServer().getPlayer(sender.getName()).getInventory().remove(item);
+	    				ItemMeta meta = item.getItemMeta();
+	    				meta.setDisplayName("Upside down slab");
+	    				item.setItemMeta(meta);
+	    				sender.getServer().getPlayer(sender.getName()).getInventory().addItem(item);
+	    				sender.sendMessage("Your requested slab is now in your hand");
+	    			}else{
+	    				if(((Player) sender).getGameMode() != GameMode.SURVIVAL){
+	    				ItemStack item1 = new ItemStack(Material.STEP);
+	    				ItemMeta meta = item1.getItemMeta();
+	    				meta.setDisplayName("Upside down slab");
+	    				item1.setItemMeta(meta);
+	    				sender.getServer().getPlayer(sender.getName()).getInventory().addItem(item1);
+	    				sender.sendMessage("Your requested slab has been added inventory.");
+	    				}else{
+	    					sender.sendMessage("You must have a slab in your hand to get anuUpside down salb in Survival");
+	    				}}
+	    			}else{
+	    				sender.sendMessage("This command can only be ran from ingame.");
+	    			}
+	    			return true;
+	    		}else{
+	    			return false;
+	    		}
+	    	}
+	    	
 }
--- src/main/java/chibill/slabs/slabs/SlabEventHandler.java
+++ src/main/java/chibill/slabs/slabs/SlabEventHandler.java
@@ -1,35 +1,24 @@
 package chibill.slabs.slabs;
 
-import org.bukkit.block.Block;
-import org.bukkit.enchantments.Enchantment;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
-import chibill.slabs.Main;
-
 public class SlabEventHandler  implements Listener {
-
-
 	
-	public SlabEventHandler() {
-		
-	}
-
+	
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event){
-		if(Main.isSlab(event.getBlockPlaced().getState().getData())){
-			if((event.getItemInHand().getEnchantmentLevel(Enchantment.DURABILITY)==1) && (event.getItemInHand().getItemMeta().getDisplayName() == "Upside down slab")){
-				SetInverted(event.getBlockPlaced());
-			}
+		
+		if(event.getBlock().getType().equals(Material.STEP) | event.getBlock().getType().equals(Material.WOOD_STEP) | event.getBlock().getType().equals(Material.STONE_SLAB2)| event.getBlock().getType().equals(Material.PURPUR_SLAB)){
+			
+			if((event.getItemInHand().getItemMeta().getDisplayName().hashCode() == "Upside down slab".hashCode())){
+				if(event.getBlockReplacedState().getType() != event.getBlockPlaced().getType()){
+				event.getBlock().getState().getBlock().setData((byte)(event.getItemInHand().getData().getData()+ 8));
+			}}
 		}
 		
-	}
-	
-	public void SetInverted(Block b){
-		int dat = b.getData() & 0x7;
-		dat |= 8;
-		b.setData((byte) dat);
 	}
 	
 }
