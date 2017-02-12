0c8365a9e894aa8e87375216343e7184ee395ec7
William Gaylord <chibill110@gmail.com>
2017-02-12 17:40:13 -0600

    Fix stupid bug in spigots code on my end.

--- src/main/java/chibill/slabs/slabs/SlabEventHandler.java
+++ src/main/java/chibill/slabs/slabs/SlabEventHandler.java
@@ -10,14 +10,14 @@ public class SlabEventHandler  implements Listener {
 	
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event){
-		
+		try{
 		if(event.getBlock().getType().equals(Material.STEP) | event.getBlock().getType().equals(Material.WOOD_STEP) | event.getBlock().getType().equals(Material.STONE_SLAB2)| event.getBlock().getType().equals(Material.PURPUR_SLAB)){
 			
 			if((event.getItemInHand().getItemMeta().getDisplayName().hashCode() == "Upside down slab".hashCode())){
 				if(event.getBlockReplacedState().getType() != event.getBlockPlaced().getType()){
 				event.getBlock().getState().getBlock().setData((byte)(event.getItemInHand().getData().getData()+ 8));
 			}}
-		}
+		}catch(Exception e){}
 		
 	}
 	
